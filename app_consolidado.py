fdf9fa892f028104c831348aa30225b59e934672
majoocharteKellanova <mariajose.ocharte@kellanova.com>
2026-03-11 12:40:26 -0600

    Update app_consolidado.py

--- app_consolidado.py
+++ app_consolidado.py
@@ -333,9 +333,7 @@ with tab2:
         df_total2["Num. Tienda"] = df_total2["Num. Tienda"].astype(int)
         df_total2["Codigo"] = df_total2["Codigo"].astype(int)
         df_total2["Número de Proveedor"] = df_total2["Número de Proveedor"].astype(int)
-        df_total2["Total"] = df_total2["Total"].astype(float)
         df_total2["Número de Pedido"] = df_total2["Número de Pedido"].astype(int)
-        df_total2["Cap. Emp."] = df_total2["Cap. Emp."].astype(float)
         
 
 
